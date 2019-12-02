bb9ed2632cbdbe7db458f21cf4a11ebc3b229fce
Stefan Niculae <stefan1niculae@gmail.com>
2019-12-01 23:55:09 -0800

    Add chosen model and configs for intervention comparison.

--- configs/generate_configs_intervention.py
+++ configs/generate_configs_intervention.py
@@ -7,50 +7,50 @@ from os import makedirs
 from configs.structure import read_config
 
 
-DEFAULT_ROOT_DIR = 'generated'
+DEFAULT_ROOT_DIR = 'intervention-comparison'
 
 fixed_params = {
-    # TODO fill in with best params
-    'algorithm': 'pg',
-    'state_representation': 'coordinates',
-    'encoder': 'fc',
-    'num_encoder_layers': 1,
-    'encoder_layer_size': 8,
-    'num_decoder_layers': 1,
-    'decoder_layer_size': 4,
-    'activation': 'relu',
-    'lr': 0.01,
-    'entropy_coef': 0.001,
-
     'scenario': '9x9,2v2,pacman-1',
-    'treasure_collection_limit': 2,
-    'time_limit': 100,
+    "treasure_collection_limit": 2,
+    'time_limit': 80,
 
-    'batch_norm': False,
-    'layer_norm': False,
+    'algorithm': 'ppo',
+    "discount": 0.96,
 
-    'max_run_time': 1,
-    'num_iterations': 250,
-    'num_transitions': 4000,
-    'batch_size': 512,
-    'num_epochs': 5,
+    'state_representation': 'grid',
+    'encoder': 'conv',
+    'encoder_layer_size': 'hardcoded',
+    'decoder_layer_size': 'hardcoded',
+    'activation': 'leaky_relu',
+    'batch_norm': True,
 
-    'log_interval': 1,
-    'save_interval': 50,
-    'eval_interval': 0,
+    "entropy_coef_milestones":   [    0,    150,   300,    400],
+    "entropy_coef_values":       [0.006,  0.003, 0.001, 0.0001],
+    "lr_milestones":             [    0,    200],
+    "lr_values":                 [0.005,  0.001],
+    "max_grad_norm": 5,
 
-    'first_no_adjustment': 25,
+    "num_iterations": 501,
+    "num_transitions": 4000,
+    "batch_size": 512,
+    "num_epochs": 8,
+
+    "log_interval": 1,
+    "eval_interval": 25,
+    "save_interval": 100,
+
+    'first_no_adjustment': 50,
 }
 
 translator = {}
 
 param_grid = {
-    'seed': [1, 2, 3],
-    'winrate_threshold': [None, .6, .7, .8],
-    'adjust_lr_to': [None, 0, 0.0001],
-    'adjust_mi_to': [None, 1, .25],
-    'adjust_uniform_to': [None, .25, .66],
-    'adjust_scripted_to': [None, .25, .66],
+    'seed': [1, 2],
+    'winrate_threshold': [None, .6, .66, .7, .8],
+    'adjust_lr_to': [None, 0],
+    'adjust_mi_to': [None, .2],
+    'adjust_uniform_to': [None, .33],
+    'adjust_scripted_to': [None, .5],
 }
 
 
@@ -64,6 +64,17 @@ def is_valid(d: dict):
     num_enabled = sum(adjustments_enabled)
     if num_enabled > 1:
         return False
+
+    if 'winrate_threshold' in d:
+        t = d['winrate_threshold']
+        if 'adjust_mi_to' in d:
+            return t == .66
+        if 'adjust_uniform_to' in d:
+            return t == .66
+        if 'adjust_scripted_to' in d:
+            return t in [.6, .7]
+        if 'adjust_lr_to' in d:
+            return t in [.6, .7, .8]
 
     return (num_enabled == 1) == ('winrate_threshold' in d)
 
@@ -114,15 +125,13 @@ if __name__ == '__main__':
     else:
         root_dir = DEFAULT_ROOT_DIR
 
-    fixed_name = 'best-model'
-    output_dir = root_dir + '/' + fixed_name
-    makedirs(output_dir, exist_ok=True)
+    makedirs(root_dir, exist_ok=True)
 
     for i, comb in enumerate(carthesian_product(param_grid)):
         name = comb_name(comb)  # before parsing
         comb = parse(comb)
 
-        path = f'{output_dir}/#{i} - {name}.json'
+        path = f'{root_dir}/#{i} - {name}.json'
         with open(path, 'w') as f:
             json.dump(comb, f, indent=2)
 
